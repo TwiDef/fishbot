b414bf35622ab6c31efe6835187c9b3940549aca
Def <deferenseo@gmail.com>
2025-07-19 10:12:36 +0300

    logging/folder_paths/fixed mse(%)

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ from skimage.io import imread
 from pynput.keyboard import Controller
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.getLogger('PIL').setLevel(logging.WARNING)
 keyboard = Controller()
 
 screenshots_folder_path = "assets/shots"
 diff_folder_path = "assets/diff"
+bobber_folder_path = "assets/bobber"
+trigger_folder_path = "assets/trigger"
 
 LEFT_FRAME_BORDER = 600
 TOP_FRAME_BORDER = 50
 
 def screenshot_frame():
-  pyautogui.screenshot(region = (LEFT_FRAME_BORDER, TOP_FRAME_BORDER, 800, 400)).save("assets/shots/s_" + str(random.randint(1, 9999)) + ".png")
+  pyautogui.screenshot(region = (LEFT_FRAME_BORDER, TOP_FRAME_BORDER, 800, 400)).save(screenshots_folder_path + "/s_" + str(random.randint(1, 9999)) + ".png")
 
 def screenshot_bobber(x_coord, y_coord, path, name):
   pyautogui.screenshot(region = (x_coord - 30, y_coord - 30, 60, 60)).save(path + name + ".png")
@@ -98,6 +101,9 @@ def delete_files_in_folder(folder_path):
         os.remove(file_path)
     except Exception:
       print(f'Ошибка при удалении файла {file_path}. {Exception}')
+
+def toFixed(num, digits=0):
+  return float(f"{num:.{digits}f}")
   
 
 # game keys binding
@@ -107,7 +113,7 @@ equip_fishing_pole = "r"
 def fishing():
   # start programm
   duration = 3
-  logging.info(f"capture to focus game window! delay {duration} seconds")
+  logging.info(f" capture to focus game window! delay {duration} seconds")
 
   while duration > 0:
     logging.info(" " + str(duration) + " seconds remaining...")
@@ -121,7 +127,7 @@ def fishing():
 
   # do screenshot frame without bobber
   time.sleep(1)
-  logging.info("screenshot without bobber has been captured")
+  logging.info(" screenshot without bobber has been captured")
   screenshot_frame()
 
   # start fishing
@@ -131,21 +137,21 @@ def fishing():
 
   # do screenshot frame with active bobber
   time.sleep(1)
-  logging.info("screenshot with bobber has been captured")
+  logging.info(" screenshot with bobber has been captured")
   screenshot_frame()
 
   # create image list
-  image_list = create_image_list("assets/shots")
+  image_list = create_image_list(screenshots_folder_path)
 
   screenshot_1 = Image.open(image_list[0])
   screenshot_2 = Image.open(image_list[1]) 
 
   res = ImageChops.difference(screenshot_1, screenshot_2)
-  logging.info("difference image has been generated")
-  res.save("assets/diff/result.jpg")
+  logging.info(" difference image has been generated")
+  res.save(diff_folder_path + "/result.jpg")
 
   #get unic colors from diff image
-  diff_image_path = "assets/diff/result.jpg"
+  diff_image_path = diff_folder_path + "/result.jpg"
   unique_colors = get_unic_colors(diff_image_path)
   colors_list = list(unique_colors)
 
@@ -158,8 +164,12 @@ def fishing():
   # find coords by target_color
   coords = find_color_coords(diff_image_path, deep_red_color)
 
-  # generated x an dy coordinates
-  x_coord, y_coord = convert_coords(*coords)
+  # generated x an ry coordinates
+  try:
+    x_coord, y_coord = convert_coords(*coords)
+  except Exception:
+    print(f"takes 1 positional argument {Exception}")
+    #print(f"{x_coord}, {y_coord}")
 
   # move mouse to dinamic coordinates
   pyautogui.moveTo(x_coord + LEFT_FRAME_BORDER, y_coord + TOP_FRAME_BORDER, duration=0.2)
@@ -169,20 +179,21 @@ def fishing():
   x, y = pyautogui.position()
 
   # catch by trigger
-  screenshot_bobber(x, y, "assets/bobber/", "base")
+  screenshot_bobber(x, y, bobber_folder_path + "/", "base")
 
   is_catch = False
   fuse_flag = 55
 
   while not is_catch:
     # clear trigger screenshots folder
-    delete_files_in_folder("assets/trigger")
-    time.sleep(0.2)
+    delete_files_in_folder(trigger_folder_path)
 
-    screenshot_bobber(x,y, "assets/trigger/", "trigger")
+    time.sleep(0.2)
+    screenshot_bobber(x,y, trigger_folder_path + "/", "trigger")
       
-    img_1 = imread("assets/bobber/base.png", as_gray=True)
-    img_2 = imread("assets/trigger/trigger.png", as_gray=True)
+    # open and convert images      
+    img_1 = imread(bobber_folder_path + "/base.png", as_gray=True)
+    img_2 = imread(trigger_folder_path + "/trigger.png", as_gray=True)
 
     img_1_resized = resize(img_1, (512, 512), anti_aliasing=True)
     img_2_resized = resize(img_2, (512, 512), anti_aliasing=True)
@@ -190,18 +201,18 @@ def fishing():
     # seek the difference between base image and trigger image (in %)
     mse = mean_squared_error(img_1_resized, img_2_resized)
     max_possible_mse = 1.0 
-    similarity_percentage = (1 - (mse / max_possible_mse)) * 100
-    #logging.info(similarity_percentage)
+    similarity_percentage = toFixed((1 - (mse / max_possible_mse)) * 100, 3)
+    logging.info(" " + str(similarity_percentage))
     fuse_flag -= 1
 
     # exit from loop (we have the diffence or waiting too much)
-    if similarity_percentage < 99.5 or fuse_flag < 1:
+    if similarity_percentage < 99.3 or fuse_flag < 1:
       is_catch = True
-      delete_files_in_folder("assets/trigger")
-      delete_files_in_folder("assets/bobber")
+      delete_files_in_folder(trigger_folder_path)
+      delete_files_in_folder(bobber_folder_path)
 
   # collect the catch
-  logging.info("collect the catch")
+  logging.info(" collect the catch")
   pyautogui.rightClick()
   time.sleep(1)
   pyautogui.click()
@@ -210,7 +221,14 @@ def fishing():
   delete_files_in_folder(diff_folder_path)
   delete_files_in_folder(screenshots_folder_path)
 
-i = int(input("How many times do you want to cast a fishing? "))
-while i > 0:
+# clear working folders
+delete_files_in_folder(diff_folder_path)
+delete_files_in_folder(screenshots_folder_path)
+delete_files_in_folder(trigger_folder_path)
+delete_files_in_folder(bobber_folder_path)
+
+# start script 
+cast_times = int(input("How many times do you want to cast a fishing? "))
+while cast_times > 0:
   fishing()
-  i -= 1
+  cast_times -= 1
